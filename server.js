1dd2528e35dfbd608195e26f21830c0e3caa073c
Trevor <ngw21001@byui.edu>
2024-05-17 12:42:00 +0200

    use express session

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongodb = require('./data/database');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const passport = require('passport');
-// const session = require('express-session');
-const session = require('cookie-session');
+const session = require('express-session');
+// const session = require('cookie-session');
 
 const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
